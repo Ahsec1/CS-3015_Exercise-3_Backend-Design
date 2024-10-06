e0e79526d9c78b967d7a1b278a1db6d244b86968
Ahsec1 <11600056@usc.edu.ph>
2024-10-07 01:33:42 +0800

    changed the max request since it contradicts to the comment

--- middleware/rateLimiterMiddleware.js
+++ middleware/rateLimiterMiddleware.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiterMiddleware = rateLimit({
     windowMs: 1 * 10 * 1000, // 10 seconds
-    max: 5, // Limit each IP to 5 requests per windowMs
+    max: 10, // Limit each IP to 5 requests per windowMs
     message: {
         error: 'Too many requests. Please try again later',
     },
