bb319e408a947213a69c4b134bbcf9eee1f52a12
Ahsec1 <11600056@usc.edu.ph>
2024-10-07 01:28:50 +0800

    changed comment since I want its ratelimite to be 10 seconds

--- middleware/rateLimiterMiddleware.js
+++ middleware/rateLimiterMiddleware.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const rateLimiterMiddleware = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
+    windowMs: 1 * 10 * 1000, // 10 seconds
     max: 5, // Limit each IP to 5 requests per windowMs
     message: {
         error: 'Too many requests. Please try again later',
